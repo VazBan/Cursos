5efe5d9f906ae273e4642eb954230f93faf2b119
Alejandro Vazquez <rvazquez9@ucol.com>
2022-09-14 21:26:01 -0500

    Logica del juego Blackjack

--- JavaScript/02-Blackjack/assets/js/juego.js
+++ JavaScript/02-Blackjack/assets/js/juego.js
@@ -3,6 +3,19 @@
 const tipos      = ['C','D','H','S'];
 const especiales = ['A','J','Q','K'];
 
+
+//Puntos Jugadores
+let puntosJugador     = 0 ;
+    puntosComputadora = 0 ;
+
+//Referencias HTML
+const btnPedir             = document.querySelector('#btnPedir');
+const btnNuevo             = document.querySelector('#btnNuevo');
+const btnDetener           = document.querySelector('#btnDetener');
+const puntosHTML           = document.querySelectorAll('small');
+const divCartasJugador     = document.querySelector('#Jugador-cartas');
+const divCartasComputadora = document.querySelector('#Computadora-cartas')
+
 //Esta funcion crea una nueva baraja
 const crearBaraja = ()=>{
     
@@ -12,38 +25,34 @@ const crearBaraja = ()=>{
             baraja.push(i + tipo);
         }
     }
-
-    for(let tipo of tipos) {
+      for(let tipo of tipos) {
         for(let especial of especiales) {
             baraja.push( especial + tipo );
         }
     }
-    /*
-    "._shuffle" es un metodo de una libreria para ordenar aleatoriamente nuestro array llamado baraja 
-    */
+    
+    //"._shuffle" es un metodo de una libreria para ordenar aleatoriamente nuestro array llamado baraja 
     baraja = _.shuffle( baraja );
     console.log( baraja );
     return baraja;
 }
+
+//Creacion de baraja
+let barajaR = crearBaraja();
  
-
 // Esta funcion me permite tomar una  carta
-
 const pedirCarta = () => {
     
     if ( barajaR.length === 0 ) {
         throw 'No hay cartas en el deck';
+        console.log(barajaR);
     }
-
-    const carta = barajaR.pop();
-    console.log(barajaR);
-    console.log(carta);
-    
+     const carta = barajaR.pop();
+     
     return carta;
 }
 
-
-//SABER EL VALOR DE LA CARTA QUITANDOLE LAS LETRAS 
+//SABER EL VALOR DE LA CARTA QUITANDOLE LAS LETRAS:2H = 2 
 const valorCarta = ( carta ) =>{
     
     const valor = carta.substring(0, carta.length-1);
@@ -51,12 +60,78 @@ const valorCarta = ( carta ) =>{
     ( valor === 'A')  ? 11 :10
     : valor * 1;
 }
-//Creacion de baraja
-let barajaR = crearBaraja();
 
-//Carta aleatoria
-const valor = valorCarta( pedirCarta() );
-console.log({valor});
+//Turno de la computadora
+
+const turnoComputadora = ( puntosMinimos ) =>{
+
+    do{
+//Puntos computadora
+    const carta = pedirCarta();
+    puntosComputadora = puntosComputadora + valorCarta(carta);
+    puntosHTML[1].innerText = puntosComputadora;
+//Mostrar cartas computadora
+    const imgCarta = document.createElement('img');
+    imgCarta.src = `assets/cartas/${ carta }.png`;
+    imgCarta.classList.add('carta');
+    divCartasComputadora.append(imgCarta);
+
+    if( puntosMinimos > 21){
+        break;
+    }
+
+    }while( ( puntosComputadora < puntosMinimos) && (puntosMinimos <= 21) );
+
+}
+
+//Eventos 
+
+//Jugador 
+btnPedir.addEventListener('click',() => {
+    const carta = pedirCarta();
+    puntosJugador = puntosJugador + valorCarta(carta);
+    puntosHTML[0].innerText = puntosJugador;
+//Mostrar cartas jugador
+    const imgCarta = document.createElement('img');
+    imgCarta.src = `assets/cartas/${ carta }.png`;
+    imgCarta.classList.add('carta');
+    divCartasJugador.append(imgCarta);
+//Logica de jugador
+    if (puntosJugador > 21 ){
+        console.warn('Lo siento,perdiste')
+        btnPedir.disabled   = true;
+        btnDetener.disabled = true;
+        turnoComputadora(puntosJugador);
+    }else if( puntosJugador === 21 ){
+        console.warn('21,genial');
+        turnoComputadora(puntosJugador);
+    }
+});
+
+btnDetener.addEventListener('click',() => {
+    
+    btnPedir.disabled   = true;
+    btnDetener.disabled = true;
+    turnoComputadora(puntosJugador);
+});
+
+btnNuevo.addEventListener('click',() => {
+    btnNuevo.disabled = true;
+   
+    const NuevoJuego= () =>{
+        puntosHTML[0,1].innerText = 0;
+    
+    
+    }
+});
+
+
+
+
+
+
+
+
 
 
 
